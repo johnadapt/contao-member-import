51d13cdf5ec31ca62c3fff46f4456b496312c958
johnadapt <2372574+johnadapt@users.noreply.github.com>
2025-08-28 17:05:14 -0700

    initial build

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['BE_MOD']['accounts']['bcs_member_import'] = [
+    'callback' => \\BrightCloudStudio\\MemberImport\\MemberImport::class,
+    'icon'     => 'system/themes/flexible/icons/mgroup.svg',
+];
